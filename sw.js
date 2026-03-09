81ca0676b6a182526340ba4bbc91cf7d6b82bc04
farleyafs-dotcom <farley_afs@hotmail.com>
2026-03-09 16:10:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-const CACHE = 'she-blocos-v1';
+const CACHE = 'she-blocos-v2';
 const ASSETS = [
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/Nome-blocos/',
+  '/Nome-blocos/index.html',
+  '/Nome-blocos/manifest.json'
 ];
 
 self.addEventListener('install', e => {
@@ -24,6 +23,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/Nome-blocos/index.html')))
   );
 });
